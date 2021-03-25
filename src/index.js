ab909d6f08b8bdab603caa40a198d888840c31af
HabibehKhavari <h.khavari1995@gmail.com>
2021-03-25 02:06:24 +0200

    install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counters from './components/counters';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Counters />,
   document.getElementById('root')
 );
 
